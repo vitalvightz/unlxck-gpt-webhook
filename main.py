d65e63c78d9ae16a5ce9a538617fdcd3bde2ddd6
vitalvightz <thevital1@protonmail.com>
2025-05-29 19:31:04 +0100

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request
 import openai
 import os
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
 app = FastAPI()
 
 @app.post("/webhook")
