a5f1dca159a79b7972ba945ec6ec1cf78ce3860c
vitalvightz <thevital1@protonmail.com>
2025-06-01 04:23:40 +0100

    Create weight_class.py

---
+++ weight_class.py
@@ -0,0 +1,35 @@
+# weight_classes.py
+
+weight_class_map = {
+    "MMA": {
+        "Flyweight": 56.7,
+        "Bantamweight": 61.2,
+        "Featherweight": 65.8,
+        "Lightweight": 70.3,
+        "Welterweight": 77.1,
+        "Middleweight": 83.9,
+        "Light Heavyweight": 93.0,
+        "Heavyweight": 120.2
+    },
+    "Boxing": {
+        "Flyweight": 50.8,
+        "Bantamweight": 53.5,
+        "Featherweight": 57.2,
+        "Lightweight": 61.2,
+        "Welterweight": 66.7,
+        "Middleweight": 72.6,
+        "Light Heavyweight": 79.4,
+        "Cruiserweight": 90.7,
+        "Heavyweight": 101.6
+    },
+    "Kickboxing": {
+        "Featherweight": 60.0,
+        "Lightweight": 63.5,
+        "Welterweight": 67.0,
+        "Middleweight": 75.0,
+        "Light Heavyweight": 81.0,
+        "Cruiserweight": 86.0,
+        "Heavyweight": 95.0,
+        "Super Heavyweight": 120.0
+    }
+}
