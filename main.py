71d0f849d020e90e7d29a0a7ff76ade192b8504c
vitalvightz <thevital1@protonmail.com>
2025-06-08 17:36:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from mindset_module import (
     classify_mental_block,
     get_mental_protocols,
     get_phase_mindset_cues,
+    get_mindset_by_phase,
 )
 from strength import generate_strength_block
 from conditioning import generate_conditioning_block
