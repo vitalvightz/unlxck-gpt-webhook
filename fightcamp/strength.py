af7c79be58520e3542db9f9931babd6db67650a1
vitalvightz <thevital1@protonmail.com>
2025-06-13 14:55:14 +0100

    Update strength.py

--- fightcamp/strength.py
+++ fightcamp/strength.py
@@ -118,9 +118,13 @@ def is_banned_exercise(name: str, tags: list[str], fight_format: str) -> bool:
     grappling_terms = {
         "wrestling",
         "wrestle",
+        "wrestler",
+        "bjj",
         "grappling",
         "grapple",
+        "grappler",
         "sprawl",
+        "sprawling",
     }
 
     if fight_format in {"boxing", "kickboxing"}:
