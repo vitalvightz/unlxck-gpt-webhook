974743c952053c560644e06a38599ad6d346965d
vitalvightz <thevital1@protonmail.com>
2026-09-05 02:14:49 +0100

    Install planner authority integrity gate

--- fightcamp/__init__.py
+++ fightcamp/__init__.py
@@ -2,9 +2,12 @@ from __future__ import annotations
 
 from .late_fight_dosage_policy import install as _install_late_fight_dosage_policy
 from .late_fight_phase_eligibility import install as _install_late_fight_phase_eligibility
+from .planner_authority_integrity import install as _install_planner_authority_integrity
 
 _install_late_fight_dosage_policy()
 _install_late_fight_phase_eligibility()
+_install_planner_authority_integrity()
 
 del _install_late_fight_dosage_policy
 del _install_late_fight_phase_eligibility
+del _install_planner_authority_integrity
